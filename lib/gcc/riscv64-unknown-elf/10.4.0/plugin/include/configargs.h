aa9ca10b4f24ef7efff885ce81174d93718c735a
light-ly <l136665@163.com>
2024-07-25 23:59:14 +0800

    upload mac xuantie elf tools

---
+++ lib/gcc/riscv64-unknown-elf/10.4.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/Users/huatuo/cys/xuantie-gnu-toolchain/riscv-gcc/configure --target=riscv64-unknown-elf --with-pkgversion='Xuantie-900 elf newlib gcc Toolchain V2.8.1 B-20240115' CXXFLAGS='-g -O2 ' --disable-libgcctf --prefix=/opt/riscv-toolchain/Xuantie-900-gcc-elf-newlib-v2.8.1 --disable-shared --enable-threads=posix --enable-languages=c,c++ --with-system-zlib --enable-tls --with-newlib --with-sysroot=/opt/riscv-toolchain/Xuantie-900-gcc-elf-newlib-v2.8.1/riscv64-unknown-elf --with-native-system-header-dir=/include --disable-libmudflap --disable-libssp --disable-libquadmath --disable-libgomp --disable-nls --disable-tm-clone-registry --src=.././riscv-gcc --enable-multilib --with-abi=lp64d --with-arch=rv64gc_zfh_xtheadc 'CFLAGS_FOR_TARGET=-Os   -mcmodel=medany' 'CXXFLAGS_FOR_TARGET=-Os   -mcmodel=medany'";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "abi", "lp64d" }, { "arch", "rv64gc_zfh_xtheadc" } };
